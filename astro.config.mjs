e9c979fa033bcc90d6750bfec12ed971e5face6d
Jeff Innes <contact.me@jeffinnes.dev>
2026-09-18 13:56:22 -0700

    fix: remove base path
    This should fix the 404s for images in styles now that the github page is served from "tucsonjs.club"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tucsonjs.github.io',
-  base: '/tucsonjs-website',
+  // base: '/tucsonjs-website',
 });
